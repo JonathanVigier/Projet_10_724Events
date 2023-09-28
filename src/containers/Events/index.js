4ce6bb29fbd98377dad7ebca3a4911a2c9bbb26b
Jonathan <jonathan.vigier@outlook.com>
2023-09-28 13:45:20 +0200

    Fix: fix the variable that filter the event list

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -11,11 +11,11 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
+    (type
+      ? data?.events?.filter((event) => event.type === type)
       : data?.events) || []
   ).filter((event, index) => {
     if (
